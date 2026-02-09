967e2e0ee688d55e120c557ea885c34346009334
Gennadii Saltyshchak <GennadyGS@gmail.com>
2026-02-09 09:38:26 +0200

    Ensure backward compatibility

--- InstallPowershell.ps1
+++ InstallPowershell.ps1
@@ -2,5 +2,6 @@ param (
   [switch] $Quiet
 )
 
+if ($Quiet) { $quietArg = "-Quiet" }
 Invoke-Expression `
-    "& { $(Invoke-RestMethod https://aka.ms/install-powershell.ps1) } -UseMSI $($Quiet ? '-Quiet' : '')"
+    "& { $(Invoke-RestMethod https://aka.ms/install-powershell.ps1) } -UseMSI $quietArg"
